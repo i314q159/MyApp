a094a7cce37a9fd91b8a6dd80e1557a24073c753
i314q159 <i314q159@outlook.com>
2026-03-04 10:06:44 +0800

    upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Serilog;
+
+namespace MyApp;
+
+class Program
+{
+    static void Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("log/a.log")
+        .CreateLogger();
+
+        Log.Information("Hello, Serilog!");
+    }
+}
